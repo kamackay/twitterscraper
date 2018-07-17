9e5360ed90ea93bb192392ed8b0c8bd78d6cd1e8
Keith MacKay <keith.mackay3@gmail.com>
2018-07-17 13:35:32 -0400

    convert a larger set of data into database

--- src/main/java/com/twitterscraper/model/DatabaseWrapper.java
+++ src/main/java/com/twitterscraper/model/DatabaseWrapper.java
@@ -7,19 +7,19 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.UpdateOptions;
 import org.bson.Document;
-import twitter4j.Status;
+import twitter4j.*;
 
 import java.util.Arrays;
 import java.util.Optional;
 
 import static com.mongodb.client.model.Filters.eq;
+import static java.util.stream.Collectors.toList;
 
 public class DatabaseWrapper {
-    private final MongoClient client;
     private final MongoCollection<Document> db;
 
     public DatabaseWrapper() {
-        client = MongoClients.create(MongoClientSettings.builder()
+        MongoClient client = MongoClients.create(MongoClientSettings.builder()
                 .applyToClusterSettings(builder ->
                         builder.hosts(Arrays.asList(
                                 new ServerAddress("localhost", 27017)
@@ -39,17 +39,71 @@ public class DatabaseWrapper {
                 .append("text", tweet.getText())
                 .append("retweetCount", tweet.getRetweetCount())
                 .append("createdAt", tweet.getCreatedAt())
-                .append("favoriteCount", tweet.getFavoriteCount());
+                .append("favoriteCount", tweet.getFavoriteCount())
+                .append("userMentionedEntities",
+                        Arrays.stream(tweet.getUserMentionEntities())
+                                .map(this::convert).collect(toList()))
+                .append("mediaEntities",
+                        Arrays.stream(tweet.getMediaEntities())
+                                .map(this::convert).collect(toList()))
+                .append("urlEntities",
+                        Arrays.stream(tweet.getURLEntities())
+                                .map(this::convert).collect(toList()))
+                .append("hashtagEntities",
+                        Arrays.stream(tweet.getHashtagEntities())
+                                .map(this::convert).collect(toList()));
 
         Optional.ofNullable(tweet.getUser()).ifPresent(user -> {
-            document.append("user", new Document("id", user.getId())
-                    .append("screenName", user.getScreenName())
-                    .append("name", user.getName())
-                    .append("friendsCount", user.getFriendsCount())
-                    .append("isVerified", user.isVerified()));
+            document.append("user", convert(user));
             document.append("username", user.getScreenName());
         });
-
         return document;
+    }
+
+    private Document convert(final URLEntity e) {
+        return new Document("expandedURL", e.getExpandedURL())
+                .append("text", e.getText())
+                .append("displayURL", e.getDisplayURL())
+                .append("end", e.getEnd())
+                .append("start", e.getStart())
+                .append("url", e.getURL());
+    }
+
+    private Document convert(final HashtagEntity e) {
+        return new Document("text", e.getText())
+                .append("start", e.getStart())
+                .append("end", e.getEnd());
+    }
+
+    private Document convert(final UserMentionEntity e) {
+        return new Document("id", e.getId())
+                .append("screenName", e.getScreenName())
+                .append("text", e.getText())
+                .append("start", e.getStart())
+                .append("end", e.getEnd())
+                .append("start", e.getStart())
+                .append("name", e.getName());
+    }
+
+    private Document convert(final MediaEntity e) {
+        return new Document("id", e.getId())
+                .append("mediaURL", e.getMediaURL())
+                .append("mediaURLHttps", e.getMediaURLHttps())
+                .append("expandedULR", e.getExpandedURL())
+                .append("type", e.getType())
+                .append("start", e.getStart())
+                .append("end", e.getEnd())
+                .append("displayURL", e.getDisplayURL())
+                .append("url", e.getURL());
+    }
+
+    private Document convert(final User u) {
+        return new Document("id", u.getId())
+                .append("screenName", u.getScreenName())
+                .append("name", u.getName())
+                .append("createdAt", u.getCreatedAt())
+                .append("followersCount", u.getFollowersCount())
+                .append("friendsCount", u.getFriendsCount())
+                .append("isVerified", u.isVerified());
     }
 }
