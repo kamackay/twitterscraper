a4be488d6717d8db6cf3138c5fea4b99e3749e9d
Keith MacKay <kemackay@cisco.com>
2020-02-26 12:49:36 -0500

    Run both server and scraper

--- src/main/java/com/twitterscraper/Main.java
+++ src/main/java/com/twitterscraper/Main.java
@@ -2,22 +2,13 @@ package com.twitterscraper;
 
 import com.twitterscraper.scraper.ScraperMain;
 import com.twitterscraper.server.ServerMain;
-import com.twitterscraper.utils.Elective;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
   public static void main(String[] args) {
-    final Elective<String> mode = Elective.ofNullable(System.getenv("MODE"));
-    switch (mode.map(String::toLowerCase).orElse("scraper")) {
-      default:
-      case "scraper":
-        log.info("Starting Scraper Mode");
-        ScraperMain.start();
-        break;
-      case "server":
-        ServerMain.start();
-        break;
-    }
+    log.info("Starting!");
+    ServerMain.start();
+    ScraperMain.start();
   }
 }
